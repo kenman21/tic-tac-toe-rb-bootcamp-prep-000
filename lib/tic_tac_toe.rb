99b0238e7705988646474a8b514c755a24632f13
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-07 23:00:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,10 +53,6 @@ def current_player(board)
   end 
 end 
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
